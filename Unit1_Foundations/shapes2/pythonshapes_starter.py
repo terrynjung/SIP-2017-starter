4a0d20ee06e624eeadd29bc54323ea3f6bef5a33
terrynjung <terrynjung06@gmail.com>
2017-07-06 12:37:08 -0700

    Update pythonshapes_starter.py

--- Unit1_Foundations/shapes2/pythonshapes_starter.py
+++ Unit1_Foundations/shapes2/pythonshapes_starter.py
@@ -6,16 +6,29 @@ t = Turtle()
 
 # Set Up your screen and starting position.
 setup(500,300)
-x_pos = -250
-y_pos = -150
+x_pos = 0
+y_pos = 0
 t.setposition(x_pos, y_pos)
 
-### Write your code below:
+sides = input("How many sides:")
+intsides = int(sides)
 
+t.pensize(5)
 
+if intsides > 0 and intsides <= 3:
+    t.pencolor("pink")
+if intsides > 3 and intsides <= 6:
+    t.pencolor("blue")
+if intsides > 6 and intsides <= 10:
+    t.pencolor("coral")
+if intsides > 10:
+    t.pencolor("cyan")
 
+t.pendown()
 
-
+for draw in range(intsides):
+    t.forward(50)
+    t.right(360/intsides)
 
 # Close window on click.
 exitonclick()
