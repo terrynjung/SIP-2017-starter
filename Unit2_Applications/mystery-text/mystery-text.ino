5055e9a1a1782d68f6f8fc333772acd86bf9f777
terrynjung <terrynjung06@gmail.com>
2017-07-12 09:29:03 -0700

    Update mystery-text.ino

--- Unit2_Applications/mystery-text/mystery-text.ino
+++ Unit2_Applications/mystery-text/mystery-text.ino
@@ -1,11 +1,28 @@
 void setup() {
   pinMode(13, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(6,OUTPUT);
 }
+
+int pin [] = {6, 9, 13};
+int dely [] = {100, 200, 300};
 
 void loop() {
-  digitalWrite(13, HIGH);   
-  delay(1000);              
-  digitalWrite(13, LOW);
-  delay(1000);              
-}
+  int s;
+  int p;
 
+for (s = 0, p = 0; s < 3, p < 3; s ++, p ++) {
+    digitalWrite(pin[s], HIGH);
+    delay(dely[p]);
+    digitalWrite(pin[s], LOW);
+    delay(dely[p]);
+//    digitalWrite(pin[1], LOW);
+//    dely[1];
+//    digitalWrite(pin[1], HIGH);
+//    dely[1];
+//     digitalWrite(pin[0], LOW);
+ //   dely[2];
+  //  digitalWrite(pin[0], HIGH);
+  //  dely[2];
+}
+}
